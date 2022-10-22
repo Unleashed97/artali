53372a6494b7c7e2e04f56adaceec68d748e330c
Nichkov Roman <nichkovroman@gmail.com>
2022-10-23 01:28:45 +0300

    feat: add modal

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -8,3 +8,30 @@ export const handleBurgerMenu = () => {
         document.body.classList.toggle('fixed')
     })
 }
+
+export const handleModal = () => {
+    const modal = document.querySelector('.modal')
+    const moreBtnList = document.querySelectorAll('.modal-open')
+    const modalClose = document.querySelector('.modal__close')
+
+    if (modal) {
+        moreBtnList.forEach((btn) =>
+            btn.addEventListener('click', () => {
+                modal.classList.add('active')
+                document.body.classList.add('fixed')
+            }),
+        )
+
+        modalClose.addEventListener('click', () => {
+            modal.classList.remove('active')
+            document.body.classList.remove('fixed')
+        })
+
+        window.addEventListener('click', (e) => {
+            if (e.target === modal) {
+                modal.classList.remove('active')
+                document.body.classList.remove('fixed')
+            }
+        })
+    }
+}
